070e75d89c1b8f297a1ccf9e4e7f3c6d87265661
Yashraj Singh Sisodiya <97979728+theyashsisodiya@users.noreply.github.com>
2024-01-22 08:56:51 +0000

    message

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Yashraj singh sisodiya",
+  title: "Devops Engineer",
+  email: "theyashsisodiya@gmail.com",
+  gitHub: "theyashsisodiya",
+  instagram: "theyashsisodiya",
+  linkedIn: "theyashsisodiya",
+  medium: "theyashsisodiya",
+  twitter: "theyashsisodiya",
+  youTube: "theyashsisodiya",
 };
 
 const primaryColor = "#4E567E";
